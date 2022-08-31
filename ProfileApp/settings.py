40c234c2304c9345335daa2784281ab3f6e49c77
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2022-08-31 06:00:18 -0700

    Update settings.py

--- ProfileApp/settings.py
+++ ProfileApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mckds)r%)0gjb&_wod6yhvq$wataj6^rn_2%3zwi!9#)pu82t7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
